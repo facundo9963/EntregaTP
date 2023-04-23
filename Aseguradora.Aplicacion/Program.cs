3f5c2ae79a9f380c90b6c526be8d9ce541900b6a
Facundo Sánchez <facundosanchez9963@gmail.com>
2023-04-23 17:56:34 -0300

    a

--- Aseguradora.Aplicacion/Program.cs
+++ Aseguradora.Aplicacion/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
