158acae8fc2e207a9c6f8111e2c31460c739e6d1
juannnacuna <juaniacu02@gmail.com>
2023-04-30 17:10:31 -0300

    probando repositorios

--- Aseguradora/Aseguradora.Consola/Program.cs
+++ Aseguradora/Aseguradora.Consola/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Aseguradora.Aplicacion;
+using Aseguradora.Repositorios;
+
+IRepositorioPoliza repoPoliza = new RepositorioPolizaTXT();
+
+var agregarPoliza = new AgregarPolizaUseCase(repoPoliza);
+var listarPolizas = new ListarPolizasUseCase(repoPoliza);
+
+agregarPoliza.Ejecutar(new Poliza() {Id=1,
+                                    IdVehiculo=3942,
+                                    Valor=65.99f,
+                                    Franquicia=33.99f,
+                                    TipoCobertura=Poliza.TipoCob.ResponsabilidadCivil,
+                                    FechaInicioVigencia=DateTime.Now,
+                                    FechaFinVigencia=DateTime.Today
+                                    });
+
+var lista = listarPolizas.Ejecutar();
+foreach (Poliza p in lista)
+{
+    Console.WriteLine(p);
+}
